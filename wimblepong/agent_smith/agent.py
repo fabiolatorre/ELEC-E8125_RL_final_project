corrected sign actor loss

wimblepong/agent_smith/agent.py
@@ -101,8 +101,8 @@ class Agent(object):
         all_done = torch.Tensor(self.done).to(self.train_device)
         self.states, self.next_states, self.actions, self.rewards, self.done = [], [], [], [], []
 
-        # # Compute discounted rewards
-        # discounted_rewards = discount_rewards(all_rewards, self.gamma)
+        # Compute discounted rewards
+        discounted_rewards = discount_rewards(all_rewards, self.gamma)
 
         # Compute state value estimates
         _, old_state_values = self.policy(all_states)
@@ -116,7 +116,7 @@ class Agent(object):
         old_state_values = old_state_values.squeeze(-1)
 
         # Estimate of state value and critic loss
-        updated_state_values = all_rewards + self.gamma * next_state_values
+        updated_state_values = discounted_rewards + self.gamma * next_state_values
         critic_loss = F.mse_loss(old_state_values, updated_state_values.detach())
 
         # # Compute advantage
@@ -124,13 +124,14 @@ class Agent(object):
 
         # Estimate advantage
         advantages = updated_state_values - old_state_values
-        advantages -= torch.mean(advantages)
-        advantages /= torch.std(advantages.detach())
+        # advantages -= torch.mean(advantages)
+        # advantages /= torch.std(advantages.detach())
 
+        # Weighted log probs
         weighted_probs = - all_actions * advantages.detach()
 
         # Compute actor and critic loss
-        actor_loss = -torch.mean(weighted_probs)  # TODO: check sign
+        actor_loss = torch.mean(weighted_probs)  # TODO: check sign
         loss = actor_loss + critic_loss
 
         loss.backward()

wimblepong/train.py
@@ -67,7 +67,7 @@ for episode in range(0, episodes):
             win1 += 1
 
         # Give reward for surviving
-        # rew1 += 0.1
+        rew1 += 0.05
 
         # Store action's outcome (so that the agent can improve its policy)
         player.store_outcome(prev_ob1, ob1, action_prob1, rew1, done)
@@ -91,7 +91,7 @@ for episode in range(0, episodes):
     average_episode_length.append(np.mean(episode_length_history[max(0, len(episode_length_history) - 100):]))
 
     if not episode % 20 and episode:
-        print("Episode {} over. Broken WR: {:.3f}. AVG reward: {}. Episode legth: {:.2f}.".format(episode, wr_array[-1], average_reward_history[-1], average_episode_length[-1]))
+        print("Episode {} over. Broken WR: {:.3f}. AVG reward: {:.3f}. Episode legth: {:.2f}.".format(episode, wr_array[-1], average_reward_history[-1], average_episode_length[-1]))
 
     if not episode % 100 and episode:
         # Save model during training