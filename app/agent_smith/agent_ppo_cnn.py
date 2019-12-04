b36db7124e2db346a2384ea1997087d8825a5e74
damianodallapiccola <damiano.dallapiccola@gmail.com>
2019-12-04 20:57:30 +0200

    added cnn to ppo

---
+++ app/agent_smith/agent_ppo_cnn.py
@@ -0,0 +1,219 @@
+import numpy as np
+import torch
+import torch.nn.functional as F
+from torch.distributions import Categorical
+import os
+import random
+from PIL import Image
+#from utils.utils import preprocess_ppo_cnn
+import copy
+
+MODEL_EPISODE = 2200
+
+class Policy(torch.nn.Module):
+    def __init__(self, action_space=3):
+        super(Policy, self).__init__()
+        self.num_frames = 2
+        self.action_space = action_space
+        self.eps_clip = 0.1
+        self.hidden_neurons = 512
+        self.reshaped_size = 3520
+        #self.input_dim = 9300 * 2
+
+        self.conv1 = torch.nn.Conv2d(2, 16, 8, 4)
+        self.conv2 = torch.nn.Conv2d(16, 32, 4, 2)
+        self.fc1 = torch.nn.Linear(3520, 256)
+        self.fc2 = torch.nn.Linear(256, self.action_space)
+
+        # self.init_weights()
+
+    # def init_weights(self):
+    #     for m in self.modules():
+    #         if type(m) is torch.nn.Linear:
+    #             torch.nn.init.normal_(m.weight)  #, -1e-3, 1e-3)
+    #             torch.nn.init.zeros_(m.bias)
+
+    def layers(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        # x = x.reshape(-1, self.reshaped_size)
+        batch_size = x.size()[0]
+        x = x.view(batch_size, -1)
+        x = F.relu(x)
+        x = self.fc1(x)
+        x = F.relu(x)
+        return self.fc2(x)
+
+
+    def forward(self, d_obs, action=None, action_prob=None, advantage=None, deterministic=False):
+        if action is None:
+            with torch.no_grad():
+                logits = self.layers(d_obs)
+                if deterministic:
+                    action = int(torch.argmax(logits[0]).detach().cpu().numpy())
+                    action_prob = 1.0
+                else:
+                    c = torch.distributions.Categorical(logits=logits)
+                    action = int(c.sample().cpu().numpy()[0])
+                    action_prob = float(c.probs[0, action].detach().cpu().numpy())
+                return action, action_prob
+
+        # # policy gradient (REINFORCE)
+        # logits = self.layers(d_obs)
+        # loss = F.cross_entropy(logits, action, reduction='none') * advantage
+        # return loss.mean()
+
+
+        # PPO
+        if self.action_space == 3:
+            vs = np.array([[1., 0., 0.], [0., 1., 0.], [0., 0., 1.]])
+        elif self.action_space == 2:
+            vs = np.array([[1., 0.], [0., 1.]])
+        ts = torch.FloatTensor(vs[action.cpu().numpy()])
+
+        logits = self.layers(d_obs)
+        r = torch.sum(F.softmax(logits, dim=1) * ts, dim=1) / action_prob
+        loss1 = r * advantage
+        loss2 = torch.clamp(r, 1 - self.eps_clip, 1 + self.eps_clip) * advantage
+        loss = -torch.min(loss1, loss2)
+        loss = torch.mean(loss)
+
+        return loss
+
+
+class Agent(object):
+    def __init__(self, player_id=1, evaluation=True):
+        # self.train_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.train_device = torch.device("cpu")
+        self.policy = Policy().to(self.train_device)
+        self.optimizer = torch.optim.Adam(self.policy.parameters(), lr=1e-4)
+        self.player_id = player_id
+        self.gamma = 0.99
+
+        self.evaluation = evaluation
+        if self.evaluation:
+            self.policy.eval()
+
+        self.states = []
+        self.actions = []
+        self.action_probs = []
+        self.rewards = []
+
+        self.discounted_rewards = []
+
+        self.pp_observation = None
+        self.previous_observation = None
+
+    def reset(self):
+        self.previous_observation = None
+
+    def reset_lists(self):
+        self.states = []
+        self.actions = []
+        self.action_probs = []
+        self.rewards = []
+
+    def get_name(self):
+        return "Agent Smith"
+
+    def load_model(self):
+        try:
+            weights = torch.load("../models/model_"+str(MODEL_EPISODE)+".mdl", map_location=torch.device('cpu'))
+            self.policy.load_state_dict(weights, strict=False)
+        except FileNotFoundError:
+            print("Model not found. Check the path and try again.")
+
+    def save_model(self, output_directory, episode=0):
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
+        f_name = "{}/model_{}.mdl".format(output_directory, episode)
+        torch.save(self.policy.state_dict(), f_name)
+
+    def episode_finished(self):
+        R = 0
+        self.discounted_rewards = []
+        for r in self.rewards[::-1]:
+            if r != 0: R = 0  # scored/lost a point in pong, so reset reward sum
+            R = r + self.gamma * R
+            self.discounted_rewards.insert(0, R)
+
+        self.discounted_rewards = torch.FloatTensor(self.discounted_rewards)
+        self.discounted_rewards = (self.discounted_rewards - self.discounted_rewards.mean()) / self.discounted_rewards.std()
+
+        self.update_policy()
+
+    def store_outcome(self, action, action_prob, reward):
+        self.states.append(self.pp_observation)
+        self.actions.append(action)
+        self.action_probs.append(action_prob)
+        self.rewards.append(reward)
+
+    def update_policy(self):
+        for _ in range(5):
+            n_batch = 20000
+            idxs = random.sample(range(len(self.actions)), min(len(self.actions), n_batch))
+            d_obs_batch = torch.cat([self.states[idx] for idx in idxs], 0)
+            action_batch = torch.LongTensor([self.actions[idx] for idx in idxs])
+            action_prob_batch = torch.FloatTensor([self.action_probs[idx] for idx in idxs])
+            advantage_batch = torch.FloatTensor([self.discounted_rewards[idx] for idx in idxs])
+            # advantage_batch = (advantage_batch - advantage_batch.mean()) / advantage_batch.std()
+
+            self.optimizer.zero_grad()
+            loss = self.policy.forward(d_obs_batch, action_batch, action_prob_batch, advantage_batch)
+            loss.backward()
+            self.optimizer.step()
+
+
+
+
+
+    def preprocess(self, observation):
+        """ prepro 200x200x3 uint8 frame into 6000 (75x80) 1D float vector """
+        observation = observation[:, 8:192]  # crop - remove 35px from start & 25px from end of image in x, to reduce redundant parts of image (i.e. after ball passes paddle)
+        observation = observation[::2, ::2] # downsample by factor of 2.
+        observation[observation == 58] = 0  # erase background (background type 1)
+        observation[observation == 43] = 0  # erase background (background type 2)
+        observation[observation == 48] = 0  # erase background (background type 3)
+        #observation[observation != 0] = 1  # everything else (paddles, ball) just set to 1. this makes the image grayscale effectively
+
+        # img = Image.fromarray(observation, 'RGB')
+        # # img.save('my.png')
+        # img.show()
+        # observation = torch.from_numpy(observation.astype(np.float32).ravel()).unsqueeze(0)
+        observation = observation[::, ::].mean(axis=-1)
+        #observation = np.expand_dims(observation, axis=-1)
+
+        if self.previous_observation is None:
+            self.previous_observation = copy.deepcopy(observation[np.newaxis, np.newaxis, :, :])
+
+        # rtn = torch.cat([self.previous_observation, observation], dim=1)
+
+        observation = observation[np.newaxis, np.newaxis, :, :]
+        stack_ob = np.concatenate((observation, self.previous_observation), axis=1)
+        #stack_ob = stack_ob.transpose(1, 3)
+        self.previous_observation = copy.deepcopy(observation)
+        rtn = torch.tensor(stack_ob, device=self.train_device, dtype=torch.float)
+        return rtn
+
+    # def preprocess(self, observation):
+    #     observation = observation[::2, ::2].mean(axis=-1)
+    #     observation = np.expand_dims(observation, axis=-1)
+    #     if self.previous_observation is None:
+    #         self.previous_observation = observation
+    #     stack_ob = np.concatenate((self.previous_observation, observation), axis=-1)
+    #     stack_ob = torch.from_numpy(stack_ob).float().unsqueeze(0)
+    #     stack_ob = stack_ob.transpose(1, 3)
+    #     self.previous_observation = observation
+    #     return stack_ob
+
+
+
+
+    def get_action(self, observation):
+        self.pp_observation = self.preprocess(observation)
+        action, action_prob = self.policy.forward(self.pp_observation, deterministic=self.evaluation)
+        if self.evaluation:
+            return action
+        else:
+            return action, action_prob
---
+++ app/train_ppo_cnn.py
@@ -0,0 +1,107 @@
+import gym
+import numpy as np
+from wimblepong import SimpleAi
+from agent_smith.agent_ppo_cnn import Agent
+from utils.utils import plot
+import os
+import torch
+
+PLOTS_DIR = os.path.abspath("./plots")
+MODELS_DIR = os.path.abspath("./models")
+
+# Make the environment
+env = gym.make("WimblepongVisualMultiplayer-v0")
+
+# Define the player IDs for both SimpleAI agents
+player_id = 1
+opponent_id = 3 - player_id
+opponent = SimpleAi(env, opponent_id)
+
+# Get dimensionalities of actions and observations
+observation_space_dim = env.observation_space.shape[-1]
+action_space_dim = env.action_space.n
+
+player = Agent(player_id, evaluation=False)
+
+# Set the names for both players
+env.set_names(player.get_name(), opponent.get_name())
+
+# Lists to keep track of rewards
+reward_history, reward_history_avg = [], []
+wr_array, wr_array_avg = [], []
+episode_length_history, episode_length_avg = [], []
+
+# Initialize winning rate
+win1 = 0
+wr_reset = 1000
+
+episode = 0
+while True:
+    if not episode % wr_reset:
+        win1 = 0  # Reset WR
+
+    # Initialize values
+    reward_sum = 0
+    episode_length = 0
+    done = False
+
+    # Resets
+    player.reset()
+    ob1, ob2 = env.reset()
+
+    while not done:
+        # Get the actions from both players
+        with torch.no_grad():
+            action1, action_prob1 = player.get_action(ob1)
+        action2 = opponent.get_action()
+        # Step the environment and get the rewards and new observations
+        (ob1, ob2), (rew1, rew2), done, info = env.step((action1, action2))
+
+        # Count the wins
+        if rew1 == 10:
+            win1 += 1
+
+        # Store action's outcome (so that the agent can improve its policy)
+        player.store_outcome(action1, action_prob1, rew1)
+
+        # Store total episode reward
+        reward_sum += rew1
+        episode_length += 1
+
+    if not episode % 10:
+        player.episode_finished()
+        player.reset_lists()
+
+    # ---PLOTTING AND SAVING MODEL---
+
+    # Update WR values for plots
+    wr_array.append(win1 / ((episode % wr_reset)+1))
+    wr_array_avg.append(np.mean(wr_array[max(0, len(wr_array)-100):]))
+
+    # Update reward values for plots
+    reward_history.append(reward_sum)
+    reward_history_avg.append(np.mean(reward_history[max(0, len(reward_history) - 100):]))
+
+    # Update episode_length values
+    episode_length_history.append(episode_length)
+    episode_length_avg.append(np.mean(episode_length_history[max(0, len(episode_length_history) - 100):]))
+
+    if not episode % 20 and episode:
+        print("Episode {} over. Broken WR: {:.3f}. AVG reward: {:.3f}. Episode legth: {:.2f}."
+              .format(episode, wr_array[-1], reward_history_avg[-1], episode_length_avg[-1]))
+
+    if not episode % 100 and episode:
+        # Save model during training
+        player.save_model(MODELS_DIR, episode)
+        print("Model saved")
+
+    if not episode % 1000 and episode:
+        # Create plot of the training performance WR
+        plot(wr_array, wr_array_avg, "WR history", "WR_history_training",
+             PLOTS_DIR, ["WR", "100-episode average"])
+
+        # Create plot of the training performance reward
+        plot(reward_history, reward_history_avg, "Reward history", "reward_history_training",
+             PLOTS_DIR, ["Reward", "100-episode average"])
+
+    episode += 1
